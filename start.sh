ffeb7e3e82486392a6b1060c94d7b52690890f75
Benjamin Deng <d.shiangyu@gmail.com>
2017-04-04 20:40:13 +0800

    update start script

--- start.sh
+++ start.sh
@@ -1,28 +1,29 @@
-#!/bin/sh
-trap 'kill (jobs -p)' EXIT SIGINT SIGTERM
+#trap 'kill (jobs -p)' EXIT SIGTERM
 
-TEMP_CRONTAB='tmp.cr'
-ORIGIN_CRONTAB='origin.cr'
-MONGODB_PATH=`pwd`/data/db
-MONGODB_LOG=`pwd`/data/mongod.log
+# TEMP_CRONTAB='tmp.cr'
+# ORIGIN_CRONTAB='origin.cr'
+# MONGODB_PATH=`pwd`/data/db
+# MONGODB_LOG=`pwd`/data/mongod.log
 
 # init database
 #mongod --dbpath $MONGODB_PATH --fork --logpath $MONGODB_LOG #&
 
 
-# workaround to register/unregister updateDB work 
-crontab -l > $ORIGIN_CRONTAB
-cp $ORIGIN_CRONTAB $TEMP_CRONTAB
-echo "*/1 * * * * `pwd`/updateDB.sh -U" > $TEMP_CRONTAB
-crontab $TEMP_CRONTAB
-fi
+# if [ False ]; then
+# # workaround to register/unregister updateDB work 
+# crontab -l > $ORIGIN_CRONTAB
+# cp $ORIGIN_CRONTAB $TEMP_CRONTAB
+# echo "*/1 * * * * `pwd`/updateDB.sh -U" > $TEMP_CRONTAB
+# crontab $TEMP_CRONTAB
+# fi
 
 # start server
 python database.py -U
 python mainServer.py &
 
-# stop routine work
-crontab origin.cr
-rm $TEMP_CRONTAB $ORIGIN_CRONTAB
-fi
-# terminate
+# if [False]; then
+# # stop routine work
+# crontab origin.cr
+# rm $TEMP_CRONTAB $ORIGIN_CRONTAB
+# fi
+# # terminate
